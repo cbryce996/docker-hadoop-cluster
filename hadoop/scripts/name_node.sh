16ebcacbb9ce95feaf05b460c9cf09a4e4b923d9
cbryce996 <c.bryce996@gmail.com>
2023-12-10 17:41:21 +0000

    Working spark and secured cluster

--- hadoop/scripts/name_node.sh
+++ hadoop/scripts/name_node.sh
@@ -2,4 +2,6 @@
 
 kinit -kt /keys/hdfs-namenode.keytab hdfs/namenode@CLUSTER
 
+echo "Y" | hdfs namenode -format
+
 hdfs namenode
--- spark/scripts/start_spark.sh
+++ spark/scripts/start_spark.sh
@@ -5,13 +5,5 @@ kinit -kt /keys/spark-client.keytab spark/client@CLUSTER
 # Start Spark Master
 $SPARK_HOME/sbin/start-master.sh
 
-# Start Spark Worker
-$SPARK_HOME/sbin/start-worker.sh spark://localhost:7077
-
-# Your additional configurations or startup scripts if needed
-# /scripts/start_hadoop.sh
-
-whoami
-
 # Keep the container running
 tail -f /dev/null
